01815d165b362d3af1b441d10ed26d13ac2e2f8d
minjung4018 <minjung4018@naver.com>
2023-07-04 17:18:59 +0900

    최종

--- js/index.js
+++ js/index.js
@@ -1,4 +1,12 @@
-let menu = document.querySelector('#collapsible')
-let check = document.querySelectorAll('.gnb > ul input')
-
-menu.addEventListener.click
+var menu = document.querySelector('#collapsible')
+var check1 = document.querySelector('#tab_menu')
+var check2 = document.querySelector('#tab_menu2')
+var check3 = document.querySelector('#tab_menu4')
+function cancelSelection() {   
+    if(!menu.cheked){
+        check1.checked = false;
+        check2.checked = false;
+        check3.checked = false;
+    }
+}
+menu.addEventListener("click", cancelSelection)
