605a5bd437d35cb478a4dcfa1b10e0d9d80e7c41
minjung4018 <minjung4018@naver.com>
2023-07-06 15:11:42 +0900

    최종

---
+++ js/main.js
@@ -0,0 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 60,
+  // freemode: true,
+  loop: true,
+  slidesPerGroup: 4,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var swiper = new Swiper(".location", {
+  slidesPerView: 5,
+  spaceBetween: 10,
+  slidesPerGroup: 5,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
