3b5f52015ea0a4d054b99cdebbb2eece089d50eb
minjung4018 <minjung4018@naver.com>
2023-07-12 10:15:21 +0900

    최종

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,23 @@
-const open = document.querySelector("#modal-open")
-const modalBox = document.querySelector("#modal-box")
-const close = document.querySelector("#close")
-open.addEventListener('click',() => {
-    modalBox.classList.add('active')
+const open01 = document.querySelector("#modal-open01")
+const Box01 = document.querySelector("#modal-box01")
+const open02 = document.querySelector("#modal-open02")
+const Box02 = document.querySelector("#modal-box02")
+const close = document.querySelector(".close")
+open01.addEventListener('click',() => {
+    Box01.classList.add('active')
 });
 close.addEventListener('click',() => {
-    modalBox.classList.remove('active')
+    Box01.classList.remove('active')
 });
-modalBox.addEventListener('click',() => {
-    modalBox.classList.remove('active')
+Box01.addEventListener('click',() => {
+    Box01.classList.remove('active')
+});
+open02.addEventListener('click',() => {
+    Box02.classList.add('active')
+});
+close.addEventListener('click',() => {
+    Box02.classList.remove('active')
+});
+Box02.addEventListener('click',() => {
+    Box02.classList.remove('active')
 });
