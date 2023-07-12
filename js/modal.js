7967bb4ebd8c97354b668c8277f9b91f80e2df82
minjung4018 <minjung4018@naver.com>
2023-07-12 17:28:03 +0900

    최종

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,19 @@
 const open01 = document.querySelector("#modal-open01")
 const Box01 = document.querySelector("#modal-box01")
-const open02 = document.querySelector("#modal-open02")
-const Box02 = document.querySelector("#modal-box02")
+const open10 = document.querySelector("#modal-open10")
+const Box10 = document.querySelector("#modal-box10")
+const open11 = document.querySelector("#modal-open11")
+const Box11 = document.querySelector("#modal-box11")
+const open12 = document.querySelector("#modal-open12")
+const Box12 = document.querySelector("#modal-box12")
+const open13 = document.querySelector("#modal-open13")
+const Box13 = document.querySelector("#modal-box13")
+const open14 = document.querySelector("#modal-open14")
+const Box14 = document.querySelector("#modal-box14")
+const open15 = document.querySelector("#modal-open15")
+const Box15 = document.querySelector("#modal-box15")
+const open16 = document.querySelector("#modal-open16")
+const Box16 = document.querySelector("#modal-box16")
 const close = document.querySelector(".close")
 open01.addEventListener('click',() => {
     Box01.classList.add('active')
@@ -12,6 +24,9 @@ close.addEventListener('click',() => {
 Box01.addEventListener('click',() => {
     Box01.classList.remove('active')
 });
+
+const open02 = document.querySelector("#modal-open02")
+const Box02 = document.querySelector("#modal-box02")
 open02.addEventListener('click',() => {
     Box02.classList.add('active')
 });
@@ -20,4 +35,88 @@ close.addEventListener('click',() => {
 });
 Box02.addEventListener('click',() => {
     Box02.classList.remove('active')
+});
+
+const open03 = document.querySelector("#modal-open03")
+const Box03 = document.querySelector("#modal-box03")
+open03.addEventListener('click',() => {
+    Box03.classList.add('active')
+});
+close.addEventListener('click',() => {
+    Box03.classList.remove('active')
+});
+Box03.addEventListener('click',() => {
+    Box03.classList.remove('active')
+});
+
+const open04 = document.querySelector("#modal-open04")
+const Box04 = document.querySelector("#modal-box04")
+open04.addEventListener('click',() => {
+    Box04.classList.add('active')
+});
+close.addEventListener('click',() => {
+    Box04.classList.remove('active')
+});
+Box04.addEventListener('click',() => {
+    Box04.classList.remove('active')
+});
+
+const open05 = document.querySelector("#modal-open05")
+const Box05 = document.querySelector("#modal-box05")
+open05.addEventListener('click',() => {
+    Box05.classList.add('active')
+});
+close.addEventListener('click',() => {
+    Box05.classList.remove('active')
+});
+Box05.addEventListener('click',() => {
+    Box05.classList.remove('active')
+});
+
+const open06 = document.querySelector("#modal-open06")
+const Box06 = document.querySelector("#modal-box06")
+open06.addEventListener('click',() => {
+    Box06.classList.add('active')
+});
+close.addEventListener('click',() => {
+    Box06.classList.remove('active')
+});
+Box06.addEventListener('click',() => {
+    Box06.classList.remove('active')
+});
+
+const open07 = document.querySelector("#modal-open07")
+const Box07 = document.querySelector("#modal-box07")
+open07.addEventListener('click',() => {
+    Box07.classList.add('active')
+});
+close.addEventListener('click',() => {
+    Box07.classList.remove('active')
+});
+Box07.addEventListener('click',() => {
+    Box07.classList.remove('active')
+});
+
+const open08 = document.querySelector("#modal-open08")
+const Box08 = document.querySelector("#modal-box08")
+open08.addEventListener('click',() => {
+    Box08.classList.add('active')
+});
+close.addEventListener('click',() => {
+    Box08.classList.remove('active')
+});
+Box08.addEventListener('click',() => {
+    Box08.classList.remove('active')
+});
+
+const open09 = document.querySelector("#modal-open09")
+const Box09 = document.querySelector("#modal-box09")
+open09.addEventListener('click',() => {
+    Box09.classList.add('active')
+});
+close.addEventListener('click',() => {
+    Box09.classList.remove('active')
+});
+Box09.addEventListener('click',() => {
+    Box09.classList.remove('active')
 });
