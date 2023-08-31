058252f8b3ce1f146bf4600b905c26b42266bab2
minjung4018 <minjung4018@naver.com>
2023-08-31 17:27:58 +0900

    ㅊㅈ

--- js/index_search.js
+++ js/index_search.js
@@ -2,11 +2,16 @@ const text = document.querySelector("#text")
 const btn = document.querySelector("button")
 console.log(text.value)
 
-// btn.addEventListener("click", function(){
-//     if(text.value == `박민정`){
-//         alert("확인")
-//     }
-//     else{
-//         alert("박민정을 검색해주세요")
-//     }
-// })
+btn.addEventListener("click", function(){
+    if(text.value == `박민정`){
+        const after = document.querySelector(".after")
+        const work = document.querySelector(".works")
+        const search = document.querySelector(".search")
+        after.classList.add("on")
+        work.classList.add("on")
+        search.classList.add("on")
+    }
+    else{
+        alert("박민정을 검색해주세요")
+    }
+})
